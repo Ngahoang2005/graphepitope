b4b7f926c573734d0e4825fc868f1c08304d2968
Ngahoang2005 <hoangthanhnga2005@gmail.com>
2026-03-27 14:59:19 +0700

    Update EGAT.py

--- EGAT.py
+++ EGAT.py
@@ -2,8 +2,19 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-
+class AE(nn.Module):
+    def __init__(self, dim_in, dim_out, hidden, dropout = 0., bias=True):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim_in, hidden, bias=bias),
+            nn.LayerNorm(hidden),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden, dim_out, bias=bias),
+            nn.LayerNorm(dim_out),
+        )
+    def forward(self, x):
+        return self.net(x)
 class GVP(nn.Module):
     """Lõi Geometric Vector Perceptron"""
     def __init__(self, in_dims, out_dims, h_dim=None):
